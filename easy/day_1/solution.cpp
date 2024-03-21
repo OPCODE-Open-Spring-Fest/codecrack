7759408362cb5dcdc10689dcd985fd64a92e4558
Biraj Sanghai <sanghaibiraj@gmail.com>
2024-03-22 01:01:37 +0530

    chore: day 1 puzzles added

---
+++ easy/day_1/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define lil long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        lil n;
+        cin >> n;
+        lil a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        sort(a, a + n);
+        lil c = 1, s = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] == a[i - 1])
+                c++;
+            else
+            {
+                if (c >= 3)
+                    s += c * (c - 1) * (c - 2) / 6;
+                if (i - c > 0)
+                    s += c * (c - 1) / 2 * (i - c);
+                c = 1;
+            }
+        }
+        if (c >= 3)
+            s += c * (c - 1) * (c - 2) / 6;
+        if (n - c > 0)
+            s += c * (c - 1) / 2 * (n - c);
+        Cout << s << endl;
+    }
+}
