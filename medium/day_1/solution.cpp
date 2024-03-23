fd4fa2bc9dc797b9684ae0fd53043a5f52022a1f
guptanshuman124 <guptaanshuman124@gmail.com>
2024-03-23 15:53:25 +0530

    using recursion

--- medium/day_1/solution.cpp
+++ medium/day_1/solution.cpp
@@ -1,1 +1,38 @@
-//Write your solution here
+#include<bits/stdc++.h>
+using namespace std;
+
+void correct(vector<int> &store,int temp,int index){
+    if(index==0 || store[index]>store[index-1]){
+        return;
+    }
+
+    if(store[index]<store[index-1]){
+        swap(store[index],store[index-1]);
+        correct(store,temp,index-1);
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        vector<int> store;
+
+        for(int i=0;i<n;i++){
+            int temp;
+            cin>>temp;
+
+            store.push_back(temp);
+            correct(store,temp,i);
+        }
+
+        for(int i=0;i<n;i++){
+            cout<<store[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
