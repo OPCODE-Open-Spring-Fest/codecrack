ea0a5c05013d359ab966fe89526ad6b53aa8238c
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-03-24 11:43:04 +0530

    Solution Of day 3 Easy Problem

--- easy/day_3/solution.cpp
+++ easy/day_3/solution.cpp
@@ -1,1 +1,31 @@
 // Write your solution here
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ;
+    cin>>a;
+    while(a--){
+        int n ;
+        cin>>n;
+        vector<int> p(n) , q(n);
+        for(int j = 0 ; j<n ; j++){
+            cin>>p[j];
+        }
+        for(int j = 0 ;j<n ; j++){
+            cin>>q[j];
+        }
+        unordered_map <int ,int> l;
+        for(int j = 0 ;j<n ;j++){
+            l[p[j]]++;
+            l[q[j]]++;
+        }
+        int curr = 0;
+        int maxl = 0;
+      for (auto& entry : l) {
+        curr = max(curr, entry.second);
+        maxl = max(maxl, entry.second);
+    }
+    cout<<maxl<<endl;
+    }
+    return 0;
+}
