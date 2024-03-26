7ff30d8f63e6e74d5133ed44f878a9be109d5807
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-03-27 01:01:46 +0530

    chore: completed day5 easy task

--- easy/day_5/solution.cpp
+++ easy/day_5/solution.cpp
@@ -1,1 +1,29 @@
-//Write your code here
+// Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        if (a[0] == 1)
+            a[0]++;
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] % a[i - 1] == 0)
+                a[i]++;
+        }
+        for (auto it : a)
+            cout << it << " ";
+        cout << endl;
+    }
+}
