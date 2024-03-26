a17e62bdffc4cf4f6aaa134c4e71575a5fd54c6c
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2024-03-26 20:04:08 +0530

    chore: time complexity nlogn

--- medium/day_5/solution.cpp
+++ medium/day_5/solution.cpp
@@ -1,31 +1,50 @@
-//Write your code here
-#include<bits/stdc++.h>
+// Write your code here
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 
-int32_t main(){
-    int n , sum=0;
-    cin>>n;
-    vector<int> a(n) , ans;
-    for(int i=0; i<n; i++) {cin>>a[i]; sum += a[i];}
-    for(int i=0; i<n; i++){
-        int k = sum - a[i];
-        for(int j=0; j<n; j++){
-            if(j!=i && 2*(a[j]) == k){
-                ans.push_back(i+1);
-                break;
+int32_t main()
+{
+    int n, sum = 0;
+    cin >> n;
+    vector<int> a(n), ans;
+    vector<pair<int, int>> vp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+        vp.push_back({a[i], i});
+    }
+    if (n <= 2)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        sort(vp.begin(), vp.end());
+        for (int i = 0; i < n; i++)
+        {
+            if (i != n - 1)
+            {
+                int k = sum - vp[i].first;
+                if (2 * vp[n - 1].first == k)
+                    ans.push_back(vp[i].second + 1);
+            }
+            else
+            {
+                int k = sum - vp[i].first;
+                if (2 * vp[n - 2].first == k)
+                    ans.push_back(vp[i].second + 1);
             }
         }
-    }
-    if(ans.size()){
-        cout<<ans.size()<<endl;
-        for(auto it : ans){
-            cout<<it<<" ";
+        sort(ans.begin(), ans.end());
+        cout << ans.size() << "\n";
+        for (auto it : ans)
+        {
+            cout << it << " ";
         }
-        cout<<endl;
+        cout << "\n";
     }
-    else{
-        cout<<0<<endl;
-    }
+    return 0;
 }
