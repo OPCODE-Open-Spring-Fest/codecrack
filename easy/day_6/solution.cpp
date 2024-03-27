1e4e1221e9e9efbd637bf5f6bde13ee1427367f7
rohityadav74 <rohitjee666@gmail.com>
2024-03-27 11:14:04 +0530

    chore: update the solution of code

--- easy/day_6/solution.cpp
+++ easy/day_6/solution.cpp
@@ -1,1 +1,13 @@
 //Write your code here
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int32_t main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int r,c,x,y;//rxc->row and column and (x,y)-> position of anton
+        cin>>r>>c>>x>>y;
+        cout<<"1"<<" "<<"1"<<" "<<r<<" "<<c<<endl;
+    }
+}
