1cbb25dfd36bc83d2fba4bc2a3dcb2b87baa914c
Omji-Kesharwani <kesharwaniomji000@gmail.com>
2024-03-30 09:15:16 +0530

    solved easy day9 problem

--- easy/day_9/solution.cpp
+++ easy/day_9/solution.cpp
@@ -1,1 +1,36 @@
-//write your code here
+#include <iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+int main()
+{
+    int n,m;
+    cin>>n;
+    cin>>m;
+    vector<int>arr;
+    int t=n;
+    while(t>0)
+    {
+        int r=t%10;
+        arr.push_back(r);
+        t=t/10;
+    }
+    sort(arr.begin(),arr.end());
+    int i;
+    for( i=0;i<arr.size();i++)
+    {
+        if(arr[i]!=0)
+        break;
+    }
+    swap(arr[0],arr[i]);
+    int numcheck=0;
+    for(int j=0;j<arr.size();j++)
+    {
+       numcheck=numcheck*10+arr[j];
+    }
+    if(numcheck==m)
+    cout<<"OK"<<endl;
+    else
+    cout<<"WRONG_ANSWER"<<endl;
+    return 0;
+}
