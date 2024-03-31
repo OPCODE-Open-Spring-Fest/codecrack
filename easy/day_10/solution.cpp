91f96d24027c9686d33145452c5f2d8bb5347742
Tanishq112005 <148210922+Tanishq112005@users.noreply.github.com>
2024-03-31 15:42:15 +0530

    feat: solved day 10 easy

--- easy/day_10/solution.cpp
+++ easy/day_10/solution.cpp
@@ -1,1 +1,36 @@
 //write your code here
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a ;
+    cin>>a;
+    while(a--){
+        int b , c;
+        cin>>b>>c;
+        int arr[b][c];
+        for(int i = 0 ; i<b ; i++){
+            for(int j = 0 ; j<c ; j++){
+                cin>>arr[i][j];
+            }
+        }
+        int count = 0;
+        int minimum = INT_MAX;
+        int sum = 0;
+        for(int i = 0 ; i<b ; i++){
+            for(int j = 0 ; j<c ; j++){
+                sum = sum + abs(arr[i][j]);
+                minimum = min(minimum , abs(arr[i][j]));
+                if(arr[i][j]<=0){
+                count ++;
+            }
+            }
+        }
+        if(count%2 == 0){
+            cout<<sum<<endl;
+        }
+        else{
+            cout<<sum - 2 * minimum<<endl;
+        }
+            }
+    return 0;
+}
