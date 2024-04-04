746fdf1832e7c9b8c73b92133a4c66a5b5228d2d
guptanshuman124 <guptaanshuman124@gmail.com>
2024-04-04 21:59:31 +0530

    changing the approach

--- medium/day_11/solution.cpp
+++ medium/day_11/solution.cpp
@@ -1,1 +1,23 @@
-//write your code here
+// code by guptaanshuman7
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int q;
+    cin>>q;
+
+    while(q--){
+        long long n;
+        cin>>n;
+        string s="";//for storing the sequence
+        long long i=1;
+
+        while(s.length()<n){
+            s+=to_string(i);
+            i++;
+        }
+
+        cout<<s[n-1]<<endl;
+    }
+}
