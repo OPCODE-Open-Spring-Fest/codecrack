e781af9e403e3f951d9a73da63e552dd72ef080c
guptanshuman124 <guptaanshuman124@gmail.com>
2024-04-05 02:59:31 +0530

    solution to medium problem of day 11

--- medium/day_11/solution.cpp
+++ medium/day_11/solution.cpp
@@ -1,1 +1,35 @@
-//write your code here
+// code by guptaanshuman7
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int q;
+    cin>>q;
+
+    while(q--){
+        long long n;
+        cin>>n;
+        
+        int num=9;//num as count_of_number having 1 digit
+        int digit=1;
+        int i=0;//counting the number at which k index conincides
+
+        // counting til the digit group is reached 
+        while(n>digit*num){
+            n-=digit*num;
+            i+=num;
+            num=num*10;
+            digit++;
+        }
+
+        // calculating final number
+        i+=ceil(n/digit); 
+
+        // finding coincing index
+        char ans=(to_string(i))[(n-1)%digit];
+
+        cout<<ans<<endl;
+
+    }
+}
