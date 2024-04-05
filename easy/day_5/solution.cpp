66aeb33f74cdb17ae0e27bda2f1c06b21b2a2ab1
[ayushkrtiwari] <tiwariayush4373@gmail.com>
2024-04-05 23:28:55 +0530

    build: solution updated

--- easy/day_5/solution.cpp
+++ easy/day_5/solution.cpp
@@ -1,1 +1,34 @@
 //Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--) 
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++) 
+        {
+            cin >> arr[i];
+            if(i % 2 == 0) 
+            {
+                arr[i] = (arr[i] % 2 == 0) ? arr[i] : arr[i] + 1;
+            } 
+            else 
+            {
+                arr[i] = (arr[i] % 2 != 0) ? arr[i] : arr[i] + 1;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
