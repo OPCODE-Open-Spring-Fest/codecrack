88e2f760c2c7312b0db64d5a975ac5809b0b7261
AKS <ayushivam22@gmail.com>
2024-04-05 16:56:18 +0530

    Solved easy question day 13

--- medium/day_8/solution.cpp
+++ medium/day_8/solution.cpp
@@ -1,1 +1,21 @@
 //Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int ans=INT_MAX;
+    for(int i=0; i<n; i++)
+    {
+        int temp=abs(a[i]);
+        if(temp < ans) ans=temp;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
