09ce4733558bebd9947c724d3537bf8082df8c38
AKS <ayushivam22@gmail.com>
2024-04-05 18:05:00 +0530

    chore: day8 medium solution

--- medium/day_8/solution.cpp
+++ medium/day_8/solution.cpp
@@ -1,4 +1,4 @@
-//Write your code here
+// Write your code here
 #include <bits/stdc++.h>
 using namespace std;
 string getsequence(string str, int idx1, int idx2) // this function returns the repeating sequence of string starting with str[i]
@@ -23,25 +23,29 @@ int main()
     string str;
     string AnsStr;
     cin >> str; // input the string
-    int i=0;
-    int MaxLength=INT_MIN;
-    while(i<str.length())//iterate over the string
+    int i = 0;
+    int MaxLength = INT_MIN;
+    while (i < str.length()) // iterate over the string
     {
-        int j=i;
-        while(j<str.length())//j represents all the indices where str[i] occurs except the first occurence
+        int j = i;
+        while (j < str.length()) // j represents all the indices where str[i] occurs except the first occurence
         {
-            j=str.find(str[i],j+1);
-            if(j==-1) break; // if there is no more occurence of str[i] is left then we break the loop
-            string temp = getsequence(str,i,j);//get the sequence of string which is repeating
-            int len=temp.length();
-            if(len > MaxLength)  //if length of current repeating sequence is greater than previously stored length
-            {               // then update max length 
-                AnsStr=temp;//store max repeating sequence in AnsStr
-                MaxLength=len;
+            j = str.find(str[i], j + 1);
+            if (j == -1)
+                break;                            // if there is no more occurence of str[i] is left then we break the loop
+            string temp = getsequence(str, i, j); // get the sequence of string which is repeating
+            int len = temp.length();
+            if (len > MaxLength) // if length of current repeating sequence is greater than previously stored length
+            {                    // then update max length
+                AnsStr = temp;   // store max repeating sequence in AnsStr
+                MaxLength = len;
             }
         }
         i++;
     }
-    cout<<AnsStr<<endl;
+    if (AnsStr.length() == 0)
+        cout << -1 << endl;
+    else
+        cout << AnsStr << endl;
     return 0;
 }
