7b77dd78b221f203e0e45c69e66972c895a66a30
[ayushkrtiwari] <tiwariayush4373@gmail.com>
2024-04-06 19:38:09 +0530

    build: solution uploaded

--- easy/day_7/solution.cpp
+++ easy/day_7/solution.cpp
@@ -1,1 +1,14 @@
 //Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int d;
+        cin>>d;
+        cout<<(1+d)*(1+2*d)<<endl;
+    }
+}
