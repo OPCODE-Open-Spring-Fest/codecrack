0beb7fdc56384b72326ab96decc07c35574c1702
ADARSHsri20004 <adarshsri619@gmail.com>
2024-04-07 19:53:49 +0530

    feat: solution added for easyday15

--- easy/day_15/solution.cpp
+++ easy/day_15/solution.cpp
@@ -1,1 +1,24 @@
-//write your code here
+//write your code 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+       ll a,b,c;
+       cin>>a>>b>>c;
+       if ((abs(a-b) + c)%2==0)
+       {
+        cout<<"Second"<<endl;
+       }
+       else
+       {
+        cout<<"First"<<endl;
+       }
+       
+    }
+    
+}
