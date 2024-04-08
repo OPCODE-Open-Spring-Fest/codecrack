1698c2c0e6a4a3a4732994f47a1c868c3c69a014
eddy1112004 <aditya2.230101008@iiitbh.ac.in>
2024-04-09 01:20:40 +0530

    feat: day14 easy solution

--- easy/day_14/solution.cpp
+++ easy/day_14/solution.cpp
@@ -1,1 +1,48 @@
 //write your code here
+#include <bits/stdc++.h>
+using namespace std;
+
+int leastk(int n, vector<int> &sequence)
+{
+    int c = 1, p = 1;
+
+    for (int k = 0; k < sequence.size(); ++k)
+    {
+        int num = sequence[k];
+        p = p * num;
+    }
+
+    for (int j = 0; j < n; ++j)
+    {
+        c = c * sequence[j];
+        p = p / sequence[j];
+
+        if (c == p)
+        {
+            return j + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> sequence(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> sequence[i];
+        }
+        int ans = leastk(n, sequence);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
