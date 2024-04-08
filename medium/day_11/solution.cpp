e2733ab047901729f583103add08f5e329a8e31a
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-04-09 01:49:10 +0530

    feat: solved day 11 medium

--- medium/day_11/solution.cpp
+++ medium/day_11/solution.cpp
@@ -1,1 +1,26 @@
 //write your code here
+// Code by Jagpreet153
+// Approach : 
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int q;
+    cin >> q;
+    while(q--) 
+    {
+        long long int k;
+        cin >> k;
+        long long int len = 1, cnt = 9, start = 1;
+        while(k > len * cnt) {
+            k -= len * cnt;
+            len++;
+            cnt *= 10;
+            start *= 10;
+        }
+        start += (k - 1) / len;
+        string s = to_string(start);
+        cout << s[(k - 1) % len] << endl;
+    }
+    return 0;
+}
