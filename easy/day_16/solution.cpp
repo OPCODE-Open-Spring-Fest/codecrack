1624336772761627806d71b8714dd0a71f15fd70
ayushcodes0 <auyshsingh94@gmail.com>
2024-04-09 12:58:03 +0530

    feat: day16 easy solution

--- easy/day_16/solution.cpp
+++ easy/day_16/solution.cpp
@@ -1,1 +1,20 @@
-//write your code here
+#include <iostream>
+#include <vector>
+using namespace std;
+    
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n,k;
+        cin>>n>>k;
+        if(n%2 != 0 && (n-k)%2 != 0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    }
+ 
+    return 0;
+}
