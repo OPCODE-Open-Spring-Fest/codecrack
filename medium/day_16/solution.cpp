27cbfd8ba5d1e134c27fa2b2d319fb5db90f5b6c
anandshukla15 <anand.230102013@iiitbh.ac.in>
2024-04-09 20:35:20 +0530

    Updated solution for medium Day 16

--- medium/day_16/solution.cpp
+++ medium/day_16/solution.cpp
@@ -1,1 +1,17 @@
 //write your code here
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+    long n;
+    cin>>n;
+    if(n%2==0)
+    {
+        long s=((n/2)+1)*((n/2)+1);
+        cout<<s<<endl;
+
+    }else{
+        long s=n*4;
+        cout<<s<<endl;
+    }
+ }
