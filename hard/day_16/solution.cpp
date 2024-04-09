3e0c97a80c0b64d9bbf811ddd53dd6f14409af55
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2024-04-09 15:02:42 +0530

    chore: solved hard day 16 task

--- hard/day_16/solution.cpp
+++ hard/day_16/solution.cpp
@@ -1,1 +1,34 @@
 //write your code here
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define ldouble long long double
+
+void solve(){
+	int n,x;  cin>>n>>x;
+	int mn = n, mx = 1e19;
+	for (int i = 0; i < 64; i++){
+		int bit = 1LL<<i;
+		if(!(n&bit) && (x&bit)){
+			cout<<-1<<endl;
+			return;
+		}
+		if((n&bit) && (x&bit)){
+			mx = min(mx, ((x>>i)<<i) + bit - 1);
+		}
+		if((n&bit) && !(x&bit)){
+			mn = max(mn, ((n>>i)<<i) + bit);
+		}
+	}
+	if(mx < mn) cout<<-1<<endl;
+	else cout<<mn<<endl;
+}
+signed main(){
+    
+    int t;  cin>>t;
+    while(t--){
+		solve();
+	}
+    return 0;
+}
