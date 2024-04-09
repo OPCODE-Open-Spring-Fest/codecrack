661170519725e0dbb83cfcc402c8cae006bef4f4
Kaizo8 <shreshthvsec@gmail.com>
2024-04-10 00:10:48 +0530

    feat: day14 easy solution updated

--- easy/day_14/solution.cpp
+++ easy/day_14/solution.cpp
@@ -34,7 +34,7 @@ int main()
         {
             int count2 = 0;
             int i = 0;
-            while (count2 < (count / 2))
+            while (count2 <(count / 2))
             {
                 if (arr[i] == 2)
                 {
