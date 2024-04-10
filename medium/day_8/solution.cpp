682a94aaf3ba22b99a0deaeb4860392da86df825
deepanshpandey108 <deepanshpandey4.2.2004@gmail.com>
2024-04-11 01:23:37 +0530

    feat: soltuion added for mediumday8

--- medium/day_8/solution.cpp
+++ medium/day_8/solution.cpp
@@ -1,1 +1,41 @@
 //Write your code here
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string func(const string& s) {
+    int n = s.length();
+    int max_length = 0;
+    int max_index = -1;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int length = 0;
+            while (j + length < n && s[i + length] == s[j + length]) {
+                ++length;
+            }
+            if (length > max_length) {
+                max_length = length;
+                max_index = i;
+            }
+        }
+    }
+
+    if (max_length == 0) {
+        return "-1";
+    } else {
+        return s.substr(max_index, max_length);
+    }
+}
+
+int main() {
+    string s;
+    cin>>s;
+
+    string result = func(s);
+    cout << result << endl;
+
+    return 0;
+}
+
