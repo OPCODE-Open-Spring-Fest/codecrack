a6788a80484f707b2d82c64c025eb6240deaa3ee
ADARSHsri20004 <adarshsri619@gmail.com>
2024-04-11 00:48:35 +0530

    feat: solution added for easyday5

--- easy/day_15/solution.cpp
+++ easy/day_15/solution.cpp
@@ -1,1 +1,32 @@
-//write your code here
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a > b)
+        {
+            cout << "First" << endl;
+        }
+        else if (b > a)
+        {
+            cout << "Second" << endl;
+        }
+        else
+        {
+            if (c % 2 == 0)
+            {
+                cout << "Second" << endl;
+            }
+            else
+            {
+                cout << "First" << endl;
+            }
+        }
+    }
+}
--- easy/day_5/solution.cpp
+++ easy/day_5/solution.cpp
@@ -1,1 +1,45 @@
 //Write your code here
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+        }
+        for (int i = 0; i <n; i++)
+        {
+            if (a[i]==1)
+            {
+               a[i]=2;
+            }
+           
+            
+        }
+        for (int i = 0; i < n-1; i++)
+        {
+            if (a[i+1]%a[i]==0)
+            {
+                a[i+1]=a[i+1] + 1;
+            }
+            
+        }
+         for (int i = 0; i < n; i++)
+        {
+            cout<<a[i]<<" ";
+        }
+        
+        cout<<endl;
+        
+    }
+    
+   
+
+}
