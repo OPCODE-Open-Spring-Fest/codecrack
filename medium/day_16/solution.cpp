952811b10e8f7d246b8c990d38f0cf98cb5af73a
ashutosh <ashutosh.2201062cs@iiitbh.ac.in>
2024-04-11 23:27:10 +0530

    chore: solved day 16 medium task

--- medium/day_16/solution.cpp
+++ medium/day_16/solution.cpp
@@ -1,1 +1,13 @@
 //write your code here
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if(n % 2 == 0)
+		cout << (n / 2 + 1)*(n / 2 + 1) << endl;
+	else 
+		cout << (n + 1)*(2 + n / 2)	<< endl;
+	return 0;
+}
