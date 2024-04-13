ac940503f1c76326267bfe0d03a3c5d46cb5aace
Kaizo8 <shreshthvsec@gmail.com>
2024-04-13 15:34:35 +0530

    feat: day 5 easy problem solved

--- easy/day_5/solution.cpp
+++ easy/day_5/solution.cpp
@@ -1,1 +1,24 @@
-//Write your code here
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        long long int prev;
+        cin >> prev;
+        cout << prev << " ";
+        for(int i = 1 ; i < n ; i++){
+            long long int temp;
+            cin >> temp;
+            if(temp%prev == 0){
+                temp++;
+            }
+            cout << temp << " ";
+            prev = temp;
+        } 
+        cout << endl;
+    }
+    return 0;
+}
