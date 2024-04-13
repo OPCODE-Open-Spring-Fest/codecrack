ee304f601e422405037883115aca586f172c5dfd
Kaizo8 <shreshthvsec@gmail.com>
2024-04-13 21:43:21 +0530

    feat: day 5 easy solution modified

--- easy/day_5/solution.cpp
+++ easy/day_5/solution.cpp
@@ -8,12 +8,18 @@ int main(){
         cin >> n;
         long long int prev;
         cin >> prev;
+        if(prev == 1){
+            prev++;
+        }
         cout << prev << " ";
         for(int i = 1 ; i < n ; i++){
             long long int temp;
             cin >> temp;
-            if(temp%prev == 0){
+            if(temp == 1){
                 temp++;
+            }
+            if(temp%prev == 0){
+                temp = temp + 1;
             }
             cout << temp << " ";
             prev = temp;
