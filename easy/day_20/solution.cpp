5a78ada94d17ea10e81d31ddfff9e527ea2a541b
Kaizo8 <shreshthvsec@gmail.com>
2024-04-14 23:17:26 +0530

    feat: day 20 easy solution

--- easy/day_20/solution.cpp
+++ easy/day_20/solution.cpp
@@ -1,1 +1,37 @@
-//write your code here
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        if (n==1){
+            cout << 1 << endl;
+            continue;
+        }
+        if(n==4){
+            cout << "3 4 1 2" << endl;
+            continue;
+        }
+        if(n%2 !=0){
+            cout << -1 << endl;
+            continue;
+        } 
+        
+        else{
+            int arr[n];
+            for(int i = 1 ; i <= n ; i++){
+                arr[i] = i;
+                if(i%2 == 0 && i!=2){
+                    swap(arr[i] , arr[i-2]);
+                }
+            }
+            for(int i = 1 ; i<=n ; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
